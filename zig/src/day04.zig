9a0445534d34a774e1b0d54caaac2558ccbd917d
Vladimir Popov <vladimir@dokwork.ru>
2024-02-23 13:52:23 +0300

    day04: solution in zig (part 2)

--- zig/src/day04.zig
+++ zig/src/day04.zig
@@ -27,37 +27,50 @@ const std = @import("std");
 ///     Card 6 has no winning numbers, so it is worth no points.
 ///
 /// So, in this example, the Elf's pile of scratchcards is worth 13 points.
+/// --- Part Two ---
+///
+/// There's no such thing as "points". Instead, scratchcards only cause you to win
+/// more scratchcards equal to the number of winning numbers you have.
+///
+/// Specifically, you win copies of the scratchcards below the winning card equal to
+/// the number of matches. So, if card 10 were to have 5 matching numbers, you would
+/// win one copy each of cards 11, 12, 13, 14, and 15.
+///
+/// Copies of scratchcards are scored like normal scratchcards and have the same
+/// card number as the card they copied. So, if you win a copy of card 10 and it has
+/// 5 matching numbers, it would then win a copy of the same cards that the original
+/// card 10 won: cards 11, 12, 13, 14, and 15. This process repeats until none of
+/// the copies cause you to win any more cards. (Cards will never make you copy a
+/// card past the end of the table.)
+///
+/// This time, the above example goes differently:
+///
+/// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+/// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+/// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+/// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+/// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+/// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+///
+///     Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+///     Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+///     Your copy of card 2 also wins one copy each of cards 3 and 4.
+///     Your four instances of card 3 (one original and three copies) have two matching numbers, so you win four copies each of cards 4 and 5.
+///     Your eight instances of card 4 (one original and seven copies) have one matching number, so you win eight copies of card 5.
+///     Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+///     Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
+///
+/// Once all of the originals and copies have been processed, you end up with 1
+/// instance of card 1, 2 instances of card 2, 4 instances of card 3, 8 instances of
+/// card 4, 14 instances of card 5, and 1 instance of card 6. In total, this example
+/// pile of scratchcards causes you to ultimately have 30 scratchcards!
 pub fn solve(file: std.fs.File, part: u8) !u32 {
-    _ = part;
+    var part1 = Part1{};
+    var part2 = Part2{};
+    var solver = if (part == 1) part1.solver() else part2.solver();
     var br = std.io.bufferedReader(file.reader());
-    return solution1(br.reader().any());
-}
-
-test solution1 {
-    const data =
-        \\ Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        \\ Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-        \\ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-        \\ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-        \\ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-        \\ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-    ;
-    var input = std.io.fixedBufferStream(data);
-    const actual = try solution1(input.reader().any());
-    try std.testing.expectEqual(13, actual);
-}
-
-test "part 1: test.txt" {
-    const file = try std.fs.cwd().openFile("../data/day04/test.txt", .{ .mode = .read_only });
-    defer file.close();
-    const actual = solve(file, 1);
-    try std.testing.expectEqual(13, actual);
-}
-
-fn solution1(reader: std.io.AnyReader) !u32 {
     var buffer: [1024]u8 = undefined;
-    var result: u32 = 0;
-    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+    while (try br.reader().readUntilDelimiterOrEof(&buffer, '\n')) |line| {
         if (line.len == 0) continue;
 
         var tokenizer = std.mem.tokenizeAny(u8, line, ":|");
@@ -70,12 +83,74 @@ fn solution1(reader: std.io.AnyReader) !u32 {
             std.debug.print("Numbers were not found in\n{s}\n", .{line});
             unreachable;
         };
-        result += try calculate(winners, numbers);
+        try solver.processCard(winners, numbers);
     }
-    return result;
+    return solver.result();
 }
 
-fn calculate(winners: []const u8, numbers: []const u8) !u32 {
+const Solver = struct {
+    const Self = @This();
+    state: *anyopaque,
+    processCardFn: *const fn (state: *anyopaque, winners: []const u8, numbers: []const u8) anyerror!void,
+    resultFn: *const fn (state: *anyopaque) u32,
+
+    pub fn processCard(self: *Self, winners: []const u8, numbers: []const u8) anyerror!void {
+        return self.processCardFn(self.state, winners, numbers);
+    }
+
+    pub fn result(self: Self) u32 {
+        return self.resultFn(self.state);
+    }
+};
+
+const Part1 = struct {
+    result: u32 = 0,
+
+    fn solver(self: *Part1) Solver {
+        return .{ .state = self, .processCardFn = processCard, .resultFn = returnResult };
+    }
+
+    fn processCard(ptr: *anyopaque, winners: []const u8, numbers: []const u8) anyerror!void {
+        var self: *Part1 = @ptrCast(@alignCast(ptr));
+        const matchedCount = try matchedCardsCount(winners, numbers);
+        if (matchedCount > 0)
+            self.result += @as(u32, 1) << @intCast(matchedCount - 1);
+    }
+
+    fn returnResult(ptr: *anyopaque) u32 {
+        const self: *Part1 = @ptrCast(@alignCast(ptr));
+        return self.result;
+    }
+};
+
+const Part2 = struct {
+    processedCards: u32 = 0,
+    cards: [250]u32 = [1]u32{1} ** 250,
+
+    fn solver(self: *Part2) Solver {
+        return .{ .state = self, .processCardFn = processCard, .resultFn = result };
+    }
+
+    fn processCard(ptr: *anyopaque, winners: []const u8, numbers: []const u8) anyerror!void {
+        var self: *Part2 = @ptrCast(@alignCast(ptr));
+        const matchedCount = try matchedCardsCount(winners, numbers);
+        for (0..matchedCount) |i| {
+            self.cards[i + self.processedCards + 1] += self.cards[self.processedCards];
+        }
+        self.processedCards += 1;
+    }
+
+    fn result(ptr: *anyopaque) u32 {
+        const self: *Part2 = @ptrCast(@alignCast(ptr));
+        var res: u32 = 0;
+        for (self.cards[0..self.processedCards]) |card| {
+            res += card;
+        }
+        return res;
+    }
+};
+
+fn matchedCardsCount(winners: []const u8, numbers: []const u8) !u32 {
     var buffer: [1024]u8 = undefined;
     var bufferedAlloc = std.heap.FixedBufferAllocator.init(&buffer);
     const alloc = bufferedAlloc.allocator();
@@ -88,11 +163,25 @@ fn calculate(winners: []const u8, numbers: []const u8) !u32 {
     }
 
     iterator = std.mem.tokenizeScalar(u8, numbers, ' ');
-    var result: u32 = 0;
+    var count: u32 = 0;
     while (iterator.next()) |number| {
         if (winnersSet.contains(number)) {
-            result = if (result == 0) 1 else result * 2;
+            count += 1;
         }
     }
-    return result;
+    return count;
+}
+
+test "part 1: test.txt" {
+    const file = try std.fs.cwd().openFile("../data/day04/test.txt", .{ .mode = .read_only });
+    defer file.close();
+    const actual = solve(file, 1);
+    try std.testing.expectEqual(13, actual);
+}
+
+test "part 2: test.txt" {
+    const file = try std.fs.cwd().openFile("../data/day04/test.txt", .{ .mode = .read_only });
+    defer file.close();
+    const actual = solve(file, 2);
+    try std.testing.expectEqual(30, actual);
 }
