d94e5ab49c93b527464fc85c2af57fbd42c0622d
Vladimir Popov <vladimir@dokwork.ru>
2025-01-15 08:30:06 +0400

    Add custom test runner

--- zig/build.zig
+++ zig/build.zig
@@ -25,11 +25,16 @@ pub fn build(b: *std.Build) void {
     // but does not run it.
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
+        .test_runner = b.path("src/test_runner.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
+    if (b.args) |args| {
+        run_unit_tests.addArgs(args);
+    }
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
